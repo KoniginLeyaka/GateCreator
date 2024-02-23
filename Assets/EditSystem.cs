72ea98945c849823c4ca428e5f00d64a4c8a6a5a
KoniginLeyaka <leoteo228@mail.ru>
2024-02-23 19:14:44 +0400

    add lmb rmb context menu

--- Assets/EditSystem.cs
+++ Assets/EditSystem.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EditSystem : MonoBehaviour
 {
     private Camera camera;
-    bool isSelected = false,isOperation = false;
+    bool isSelected = false,isOperation = false,isActiveRmbMenu = false;
     private float moveY,moveX;
     private float storage;
     GameObject selectedObject = null;
+    [SerializeField] GameObject rmbMenu;
+    [SerializeField] GameObject lmbMenu;
+    [SerializeField] Button resizeButton;
 
     private void Awake()
     {
         camera = Camera.main;
+        rmbMenu.SetActive(false);
+        lmbMenu.SetActive(false);
     }
 
     void Update()
     {
         moveY = Input.GetAxis("Mouse Y") * (Time.deltaTime * 150);
         moveX = Input.GetAxis("Mouse X") * (Time.deltaTime * 150);
-        if (Input.GetMouseButtonDown(0))
+        Vector3 mousePos = Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0)) // Выделение объекта ЛКМ
         {
-            Vector3 mousePos = Input.mousePosition;
+            Vector3 vector = new Vector3(30, 0);
             Ray ray = camera.ScreenPointToRay(mousePos);
+
             if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 if (hit.collider != null)
@@ -34,10 +43,13 @@ public class EditSystem : MonoBehaviour
                         isSelected = true;
                         Debug.Log("Объект выбран: " + hit.collider.gameObject.name);
                         selectedObject = hit.collider.gameObject;
+                        lmbMenu.transform.position = mousePos + vector;
+                        lmbMenu.SetActive(true);
                     }
                     else if (layer == 6 && isSelected && !isOperation)
                     {
                         isSelected = false;
+                        lmbMenu.SetActive(false);
                         Debug.Log("Выделение объекта убрано: " + hit.collider.gameObject.name);
                         selectedObject = null;
                     }
@@ -48,6 +60,26 @@ public class EditSystem : MonoBehaviour
                 isOperation = false;
             }
         }
+
+        if (Input.GetMouseButtonDown(1) && isSelected)
+        {
+            Ray ray = camera.ScreenPointToRay(mousePos);
+            Vector3 vector = new Vector3(0, -150);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                if (hit.collider != null && hit.collider.gameObject == selectedObject && !isActiveRmbMenu)
+                {
+                    rmbMenu.transform.position = Input.mousePosition + vector;
+                    rmbMenu.SetActive(true);
+                    isActiveRmbMenu = true;
+                } else if ((hit.collider == null || hit.collider.gameObject != selectedObject || hit.collider.gameObject == selectedObject) && isActiveRmbMenu)
+                {
+                    rmbMenu.SetActive(false);
+                    isActiveRmbMenu = false;
+                }
+            }
+        }
+
         if (Input.GetKeyDown(KeyCode.G))
         {
             Vector3 mouseStartPos = Input.mousePosition;
@@ -55,20 +87,24 @@ public class EditSystem : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.S) && isSelected && !isOperation)
         {
-            Debug.Log("c");
-            isOperation = true;
             StartCoroutine(resize());
             if (Input.GetKeyDown(KeyCode.Escape)) isOperation = false;
         }
     }
     private IEnumerator resize()
     {
-        while(isOperation)
+        Debug.Log("resize");
+        isOperation = true;
+        while (isOperation)
         {
             storage += moveY + moveX;
             selectedObject.transform.localScale = new Vector3(storage, storage, storage);
             yield return new WaitForSeconds(0.02f);
         }
+    }
+    public void helpResizeFunction()
+    {
+        StartCoroutine(resize()); 
     }
     private void ObjectMove(ref GameObject editObject, Vector3 mouseStartPos)
     {
